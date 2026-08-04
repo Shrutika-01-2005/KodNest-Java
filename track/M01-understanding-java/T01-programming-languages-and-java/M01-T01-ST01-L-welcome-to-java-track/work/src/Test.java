4d3ae5ff6918a4cd49650c12b200a13e55aeac15
Shrutika Patil <shrutikapatil3692@gmail.com>
2026-08-04 11:42:04 +0530

    Days

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Test.java
@@ -0,0 +1,9 @@
+
+class Test {
+
+    public static void main(String[] args) {
+        //Print your name
+        System.out.println("Shrutika");
+
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,6 @@ public class WelcomeToJava {
     public static void main(String[] args) {
         //Write your code here
         System.out.println("Welcome to Java!");
+
     }
 }
