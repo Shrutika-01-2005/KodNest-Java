3738033f28b9d5d2ee3323d84cd2fc3d7169ea09
Shrutika Patil <shrutikapatil3692@gmail.com>
2026-08-04 12:34:06 +0530

    number checking

---
+++ practiceprg/Number.java
@@ -0,0 +1,16 @@
+
+class Number {
+
+    public static void main(String[] args) {
+        int a = 25;
+        if (a > 0) {
+            System.out.println("Number is positive");
+        } else if (a < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+
+    }
+
+}
