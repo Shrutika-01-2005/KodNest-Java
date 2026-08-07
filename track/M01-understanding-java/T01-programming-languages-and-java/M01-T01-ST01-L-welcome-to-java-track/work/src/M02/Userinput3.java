93108c654b44ae7afd4ff4f333e97b7f2bada034
Shrutika Patil <shrutikapatil3692@gmail.com>
2026-08-07 12:10:06 +0530

    Taking User Input Char

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/M02/Userinput3.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class UserInput3 {
+
+    void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter char : "); //Raja
+        char ch = scan.next().charAt(2);
+        System.out.println(ch);
+    }
+}
