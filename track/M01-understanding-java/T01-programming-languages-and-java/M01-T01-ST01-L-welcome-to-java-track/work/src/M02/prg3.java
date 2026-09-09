63c0a04871dc2035e6f73bc9c219bcec0320a008
Shrutika Patil <shrutikapatil3692@gmail.com>
2026-09-09 16:50:17 +0530

    ADD

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/M02/prg3.java
@@ -0,0 +1,9 @@
+public class prg3{
+    public static void main(String[] args){
+
+        String s1 = "Java";
+        String res = s1.toUpperCase();
+        System.out.println(res);
+        System.out.println(s1);
+    }
+}
