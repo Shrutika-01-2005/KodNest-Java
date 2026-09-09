ec71de567f780ab38337afdabdc31b678e431cf9
Shrutika Patil <shrutikapatil3692@gmail.com>
2026-09-09 17:05:09 +0530

    ADD

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/M02/prg4.java
@@ -0,0 +1,16 @@
+
+public class prg4 {
+
+    public static void main(String[] args) {
+
+        String s1 = "Raja";
+        String s2 = "Rani";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+
+        String t1 = "Anu";
+        String t2 = "Banu";
+        String res = t1.concat(t2);
+        System.out.println(res);
+    }
+}
